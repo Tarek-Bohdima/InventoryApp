7f4b17245fa1505aefd6d4ecb28dd4052d253910
Tarek <tarek.buhdeima@gmail.com>
2018-07-04 07:19:01 +0300

    first commit

---
+++ app/src/main/java/com/example/android/inventoryapp/data/ProductContract.java
@@ -0,0 +1,113 @@
+/*
+
+ PROJECT LICENSE
+
+ This project was submitted by Tarek Bohdima as part of the Android Basics Nanodegree At Udacity.
+
+ As part of Udacity Honor code, your submissions must be your own work, hence
+ submitting this project as yours will cause you to break the Udacity Honor Code
+ and the suspension of your account.
+
+ Me, the author of the project, allow you to check the code as a reference, but if
+ you submit it, it's your own responsibility if you get expelled.
+
+ Copyright (c) 2018  Tarek Bohdima
+
+
+ MIT License
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
+ */
+
+package com.example.android.inventoryapp.data;
+
+/**
+ * Created by Tarek on 04-Jul-18.
+ */
+
+import android.provider.BaseColumns;
+
+/**
+ * API Contract for the Inventory App.
+ */
+public class ProductContract {
+
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+    private ProductContract(){}
+
+    /**
+     * Inner class that defines constant values for the products database table.
+     * Each entry in the table represents a single product.
+     */
+
+    public static final class ProductEntry implements BaseColumns{
+
+        /** Name of database table for products */
+        public final static String TABLE_NAME = "products";
+
+        /**
+         * Unique ID number for the product (only for use in the database table).
+         *
+         * Type: INTEGER
+         */
+        public final static String _ID = BaseColumns._ID;
+
+        /**
+         * Name of the product.
+         *
+         * Type: TEXT
+         */
+        public final static String COLUMN_PRODUCT_NAME = "product name";
+
+        /**
+         * Price of the product.
+         *
+         * Type INTEGER
+         */
+        public final static String COLUMN_PRODUCT_PRICE = "price";
+
+        /**
+         * Products' quantity.
+         *
+         * Type: INTEGER
+         */
+        public final static String COLUMN_PRODUCT_QUANTITY = "quantity";
+
+        /**
+         * Supplier name.
+         *
+         * Type: TEXT
+         */
+        public final static String COLUMN_SUPPLIER_NAME = "supplier name";
+
+        /**
+         * Supplier phone
+         *
+         * Type: TEXT
+         */
+        public final static String COLUMN_SUPPLIER_PHONE_NUMBER = "supplier phone number";
+
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/android/inventoryapp/data/ProductDbHelper.java
@@ -0,0 +1,83 @@
+/*
+
+ PROJECT LICENSE
+
+ This project was submitted by Tarek Bohdima as part of the Android Basics Nanodegree At Udacity.
+
+ As part of Udacity Honor code, your submissions must be your own work, hence
+ submitting this project as yours will cause you to break the Udacity Honor Code
+ and the suspension of your account.
+
+ Me, the author of the project, allow you to check the code as a reference, but if
+ you submit it, it's your own responsibility if you get expelled.
+
+ Copyright (c) 2018  Tarek Bohdima
+
+
+ MIT License
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
+ */
+
+package com.example.android.inventoryapp.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Tarek on 04-Jul-18.
+ */
+public class ProductDbHelper extends SQLiteOpenHelper {
+
+    public static final String LOG_TAG = ProductDbHelper.class.getSimpleName();
+
+    /** Name of the database file */
+    private static final String DATABASE_NAME = "inventory.db";
+
+    /**
+     * Database version. If you change the database schema, you must increment the database version.
+     */
+    private static final int DATABASE_VERSION = 1;
+
+    /**
+     * Constructs a new instance of {@link ProductDbHelper}.
+     *
+     * @param context of the app
+     */
+    public ProductDbHelper(Context context) {
+        super(context,DATABASE_NAME,null,DATABASE_VERSION);
+    }
+
+    /**
+     * This is called when the database is created for the first time.
+     */
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // Create a String that contains the SQL statement to create the pets table
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
